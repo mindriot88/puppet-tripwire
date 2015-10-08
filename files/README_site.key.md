44df8013bee26e5a01cdb921dbbc7f6da06d5bd1
mindriot88 <jason.pereira@sohonet.com>
2015-10-08 14:26:39 +0100

    Jason - Module Rewrite, fixes include the ability to correctly update tw cfg and pol files on change and a rework of the local and site key deployment

--- files/README_site.key.md
+++
@@ -1,5 +0,0 @@
-You must manually create your site.key file.
-
-    cd /etc/puppet/modules/tripwire/files
-    twadmin --generate-keys --site-keyfile ./site.key
-
--- manifests/init.pp
+++ manifests/init.pp
@@ -13,6 +13,10 @@
 #   The passphrase for the local.key file.
 #   Default: none
 #
+# [*tripwire_emailto*]
+#   An array of email addresses used for tripwires email reporting.
+#   Default: undef
+#
 # === Actions:
 #
 # Installs tripwire.
@@ -25,13 +29,14 @@
 # === Sample Usage:
 #
 #  class { 'tripwire':
-#    tripwire_site  = 'sitePassPhrase',
-#    tripwire_local = 'nodePassPhrase',
+#    tripwire_site  => 'sitePassPhrase',
+#    tripwire_local => 'nodePassPhrase',
+#    tripwire_emailto => [ "joe@example.com", "jane@example.com" ];
 #  }
 #
 # === Authors:
 #
-# Mike Arnold <mike@razorsedge.org>
+# Mike Arnold <mike@razorsedge.org>, Jason Pereira <jason.pereira@sohonet.com>
 #
 # === Copyright:
 #
@@ -39,20 +44,12 @@
 #
 class tripwire (
   $tripwire_site,
-  $tripwire_local
+  $tripwire_local,
+  $tripwire_emailto = undef,
 ) inherits tripwire::params {
+
   package { 'tripwire':
     ensure => 'present',
-  }
-
-  file { 'site.key':
-    ensure  => 'present',
-    mode    => '0600',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['tripwire'],
-    path    => '/etc/tripwire/site.key',
-    source  => 'puppet:///modules/tripwire/site.key',
   }
 
   file { 'twcfg.txt':
@@ -62,7 +59,7 @@ class tripwire (
     group   => 'root',
     require => Package['tripwire'],
     path    => '/etc/tripwire/twcfg.txt',
-    source  => 'puppet:///modules/tripwire/twcfg.txt',
+    content => template("tripwire/twcfg.txt.erb"),
   }
 
   file { 'twpol.txt':
@@ -70,24 +67,11 @@ class tripwire (
     mode    => '0640',
     owner   => 'root',
     group   => 'root',
-    require => [ Package['tripwire'], Class['lsb'], ],
+    require => Package['tripwire'],
     path    => '/etc/tripwire/twpol.txt',
-    source  => [
-      "puppet:///modules/tripwire/twpol.txt-${::fqdn}",
-      "puppet:///modules/tripwire/twpol.txt-${::operatingsystem}-${::lsbmajdistrelease}-${::architecture}",
-      "puppet:///modules/tripwire/twpol.txt-${::operatingsystem}-${::lsbmajdistrelease}",
-      "puppet:///modules/tripwire/twpol.txt-${::operatingsystem}",
-      'puppet:///modules/tripwire/twpol.txt',
+    content  => [
+	template("tripwire/twpol.txt.erb"),
     ],
-  }
-
-  file { "${::fqdn}.twd":
-    ensure  => 'link',
-    target  => '/var/lib/tripwire/tripwire.twd',
-    owner   => 'root',
-    group   => 'root',
-    require => Exec['tripwire-init'],
-    path    => "/var/lib/tripwire/${::fqdn}.twd",
   }
 
   file { '/etc/tripwire':
@@ -99,46 +83,69 @@ class tripwire (
     path    => '/etc/tripwire',
   }
 
-  file { 'tripwire-setup-keyfiles':
-    ensure  => 'present',
-    mode    => '0750',
-    owner   => 'root',
-    group   => 'root',
-    require => Package['tripwire'],
-    path    => '/etc/tripwire/tripwire-setup-keyfiles',
-    source  => 'puppet:///modules/tripwire/tripwire-setup-keyfiles',
-  }
-
-  exec { 'tripwire-setup-keyfiles':
-    command     => '/etc/tripwire/tripwire-setup-keyfiles',
-    creates     => '/etc/tripwire/local.key',
-    environment => [
-      "TWLOCALPASS=${tripwire_local}",
-      "TWSITEPASS=${tripwire_site}",
+  exec { 'tripwire-gen-key':
+    command   => "/bin/rm /etc/tripwire/site.key /etc/tripwire/${::hostname}-local.key && /usr/sbin/twadmin --generate-keys --local-passphrase ${tripwire_local} --site-passphrase ${tripwire_site} --site-keyfile /etc/tripwire/site.key --local-keyfile /etc/tripwire/${::hostname}-local.key",
+    creates => [
+	"/var/lib/tripwire/${::hostname}.twd",
     ],
     require     => [
-      File['tripwire-setup-keyfiles'],
-      File['twpol.txt'],
-      File['twcfg.txt'],
-      File['site.key'],
+	File['twpol.txt'],
+	File['twcfg.txt'],
     ],
+    refreshonly => false,
+    timeout   => 10000,
+    logoutput => on_failure,
+  }
+
+  exec { 'tripwire-create-cfg':
+    command   => "/usr/sbin/twadmin --create-cfgfile -S /etc/tripwire/site.key -Q ${tripwire_site}  /etc/tripwire/twcfg.txt",
+    require   => [
+        File['twpol.txt'],
+        File['twcfg.txt'],
+    ],
+    subscribe  => [
+	Exec['tripwire-gen-key'],
+	File['twcfg.txt'],
+    ],
+    refreshonly => true,
+    timeout   => 10000,
+    logoutput => on_failure,
+  }
+
+  exec { 'tripwire-create-polfile':
+    command   => "/usr/sbin/twadmin --create-polfile -e /etc/tripwire/twpol.txt",
+    require   => [
+        File['twpol.txt'],
+        File['twcfg.txt'],
+    ],
+    subscribe  => Exec['tripwire-gen-key'],
+    refreshonly => true,
+    timeout   => 10000,
+    logoutput => on_failure,
   }
 
   exec { 'tripwire-init':
     command   => "/usr/sbin/tripwire --init --local-passphrase ${tripwire_local} --quiet",
     creates   => '/var/lib/tripwire/tripwire.twd',
-    #path      => '/sbin:/usr/sbin:/usr/local/sbin',
-    require   => Exec['tripwire-setup-keyfiles'],
+    refreshonly => true,
+    require   => [
+	Exec['tripwire-gen-key'],
+	Exec['tripwire-create-cfg'],
+	Exec['tripwire-create-polfile'],
+    ],
+    subscribe  => Exec['tripwire-create-polfile'],
     timeout   => 10000,
     logoutput => on_failure,
   }
 
-#  exec { 'tripwire-update-policy':
-#    command     => "/usr/sbin/tripwire --update-policy -Z low --local-passphrase $tripwire_local --site-passphrase $tripwire_site --quiet /etc/tripwire/twpol.txt",
-#    cwd         => '/etc/tripwire',
-#    refreshonly => 'true',
-#    subscribe   => File['twpol.txt'],
-#    timeout     => 10000,
-#    logoutput   => on_failure,
-#  }
+  exec { 'tripwire-update-policy':
+    command     => "/usr/sbin/tripwire --update-policy --secure-mode low --local-passphrase $tripwire_local --site-passphrase $tripwire_site --quiet /etc/tripwire/twpol.txt",
+    cwd         => '/etc/tripwire',
+    refreshonly => 'true',
+    subscribe   => File['twpol.txt'],
+    onlyif	=> "/usr/bin/test -e /var/lib/tripwire/${::hostname}.twd",
+    timeout     => 10000,
+    logoutput   => on_failure,
+  }
+
 }
